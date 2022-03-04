547e332e38c3ead99e80cf0ea561278306a3e0f7
Krystian Kobel <krystian.kobel@microsoft.wsei.edu.pl>
2022-03-04 17:25:10 +0100

    weighted GPAs support and override CalculateStudentStatistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,7 +6,7 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) : base(name)
+        public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
             Type = Enums.GradeBookType.Ranked;
         }
@@ -26,6 +26,14 @@ namespace GradeBook.GradeBooks
             else
             {
                 base.CalculateStatistics();
+            }
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5) Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+            {
+                base.CalculateStudentStatistics(name);
             }
         }
     }
